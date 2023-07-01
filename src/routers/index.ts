56b8c2a65030e4198357f6883997198bbb32b1f5
HAdriney <hadrineytaylors@gmail.com>
2023-07-01 18:41:42 -0300

    todas as bases preparadas

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 import * as homeController from '../controllers/homeController';
+import * as loginController from '../controllers/loginController';
+import * as registerController from '../controllers/registerController';
+import * as profileController from '../controllers/profileController';
+
 const router = Router();
 
 router.get('/', homeController.home);
+router.get('/login', loginController.login);
+router.get('/register', registerController.register);
+router.get('/profile', profileController.profile);
 
 export default router;
