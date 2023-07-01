aa118f13399b9e1337b77c9a483892b841cc1953
HAdriney <hadrineytaylors@gmail.com>
2023-07-01 17:24:16 -0300

    criando controller fazendo o primeiro view

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
-import express from "express";
+import express from 'express';
 import dotenv from 'dotenv';
-
+import path from 'path';
+import mustache from 'mustache-express';
+import mainRouter from './routers/index';
 dotenv.config();
 const server = express();
 
-server.listen(process.env.PORT)
+server.use(express.static(path.join(__dirname, '../public'))); //arquivos staticos
+server.use(express.urlencoded({ extended: true })); //middleware
+
+server.set('view engine', 'mustache'); //engine de view
+server.set('views', path.join(__dirname, 'views')); // caminho da onde o engine de view vai renderizar
+server.engine('mustache', mustache()); //colocando o engine em uso
+
+server.use(mainRouter);
+server.listen(process.env.PORT);
