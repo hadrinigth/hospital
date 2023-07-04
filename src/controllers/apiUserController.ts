eee836f1fb4c02c3e2a7cf0ba108ef909b59fc02
HAdriney <hadrineytaylors@gmail.com>
2023-07-04 18:11:24 -0300

    troca de send para json

--- src/controllers/apiUserController.ts
+++ src/controllers/apiUserController.ts
@@ -1,5 +1,40 @@
 import { Request, Response } from 'express';
+import patientModel from '../models/patientModel';
 
 export const ping = async (req: Request, res: Response) => {
-	res.json({ pong: true });
+	res.json({ pong: true }); // teste
+};
+
+export const createPatient = async (req: Request, res: Response) => {
+	try {
+		let { name, dateBirth, numSus } = req.body; //pego os dados iniciais para cria o patient
+		let newPatient = new patientModel({
+			//preencho o model comas infos
+			infos: {
+				name,
+				dateBirth,
+				numSus,
+				entry: new Date(),
+			},
+			//sera preenchida ainda
+			symptoms: {
+				diabetes: false,
+				allergies: [],
+			},
+			diagnosis: {
+				dengue: false,
+				covid: false,
+				lombriga: false,
+			},
+			uti: false,
+		});
+		await newPatient.save();
+		res.json({ success: true, patient: newPatient });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			success: false,
+			error: 'Erro ao cadastrar paciente',
+		});
+	}
 };
--- src/routers/apiRouter.ts
+++ src/routers/apiRouter.ts
@@ -4,5 +4,6 @@ import * as apiUserController from '../controllers/apiUserController';
 const router = Router();
 
 router.get('/api/ping', apiUserController.ping);
+router.post('/api/patient', apiUserController.createPatient);
 
 export default router;
