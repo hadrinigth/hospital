224ad63c1adc1ca9c9e03ee49764b2682e4874bf
HAdriney <hadrineytaylors@gmail.com>
2023-07-05 19:14:55 -0300

    att de visual

--- src/routers/apiRouter.ts
+++ src/routers/apiRouter.ts
@@ -4,6 +4,7 @@ import * as apiUserController from '../controllers/apiUserController';
 const router = Router();
 
 router.get('/api/ping', apiUserController.ping);
-router.post('/api/patient', apiUserController.createPatient);
+router.post('/api/patient', apiUserController.createPatient); //criar paciente
+router.post('/api/patient', apiUserController.updatePatient); //atualizar pacientes
 
 export default router;
